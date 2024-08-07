58c852921431e6ae10bae3dccd8905fdc2e84bf0
patresio <patresio@gmail.com>
2024-08-07 19:47:33 -0300

    movies repository and service

---
+++ movies/repository.py
@@ -0,0 +1,34 @@
+import requests
+import streamlit as st
+
+from login.service import logout
+
+
+class MovieRepository:
+
+    def __init__(self):
+        self.__base_url = "https://patresio.pythonanywhere.com/api/v1/"
+        self.__movies_url = f"{self.__base_url}movies/"
+        self.__headers = {"Authorization": f"Bearer {st.session_state.token}"}
+
+    def get_movies(self):
+        response = requests.get(self.__movies_url, headers=self.__headers)
+        if response.status_code == 200:
+            return response.json()
+        if response.status_code == 401:
+            logout()
+            return None
+        raise Exception(
+            "Erro ao obter dados da API. Status code: " + str(response.status_code)
+        )
+
+    def create_movie(self, movie):
+        response = requests.post(self.__movies_url, headers=self.__headers, data=movie)
+        if response.status_code == 201:
+            return response.json()
+        if response.status_code == 401:
+            logout()
+            return None
+        raise Exception(
+            "Erro ao obter dados da API. Status code: " + str(response.status_code)
+        )
